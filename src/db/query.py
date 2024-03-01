5e4f48f7620d22a6e1755c2fc78374358687dfd0
Thorsten Klein <tk@thklein.io>
2024-03-01 16:07:35 +0100

    change: use custom QueryResponse Model

--- src/db/query.py
+++ src/db/query.py
@@ -1,3 +1,7 @@
+import os
+from typing import List
+from pydantic import BaseModel
+
 from .errors import DatasetDoesNotExistError
 from .main import get_vector_store, vector_store_exists
 
@@ -13,6 +17,19 @@ from llama_index.embeddings.openai import OpenAIEmbedding, OpenAIEmbeddingModelT
 from llama_index.core.retrievers import VectorIndexRetriever
 
 from llama_index.core.query_engine import RetrieverQueryEngine
+
+
+class QueryResponseSourceNode(BaseModel):
+    filename: str
+    filetype: str
+    page: str
+    last_modified_date: str
+    document_title: str
+
+
+class QueryResponse(BaseModel):
+    response: str
+    sources: List[QueryResponseSourceNode] | None
 
 
 def query(
@@ -42,4 +59,17 @@ def query(
         response_synthesizer=get_response_synthesizer(),  # TODO: optimize response_mode
     )
 
-    return str(query_engine.query(prompt))  # TODO: use pydantic response
+    response = query_engine.query(prompt)
+
+    sources: List[QueryResponseSourceNode] = [
+        QueryResponseSourceNode(
+            filename=os.path.basename(node.metadata.get("file_name", "")),
+            filetype=node.metadata.get("file_type", ""),
+            page=node.metadata.get("page_label", ""),
+            last_modified_date=node.metadata.get("last_modified_date", ""),
+            document_title=node.metadata.get("document_title", ""),
+        )
+        for node in response.source_nodes
+    ]
+
+    return QueryResponse(response=str(response), sources=sources)
